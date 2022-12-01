b9a019cdb38b5c69b8cd3b07bea7c85220647136
Blaz Kranjc <blaz@kranjc.xyz>
2022-12-01 22:13:11 +0100

    Simplify top cache

--- Day1.hs
+++ Day1.hs
@@ -5,14 +5,13 @@
 -}
 
 top3sums :: String -> [Int]
-top3sums s = uncurry top3 $ foldl update ([0, 0, 0], 0) $ lines s
+top3sums s = uncurry tops $ foldl update ([0, 0, 0], 0) $ lines s
   where
-    top3 as@[a1, a2, a3] v
-      | a1 < v = [v, a1, a2]
-      | a2 < v = [a1, v, a2]
-      | a3 < v = [a1, a2, v]
-      | otherwise = as
-    update (as, v) "" = (top3 as v, 0)
+    tops [] _ = []
+    tops a@(h:t) v
+      | h < v = v : take (length t) a
+      | otherwise = h : tops t v
+    update (as, v) "" = (tops as v, 0)
     update (as, v) s = (as, v + read s :: Int)
 
 main :: IO ()
