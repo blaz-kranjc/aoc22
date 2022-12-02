1ac31a056dde34437dfcded9b64986ce5499de12
Blaz Kranjc <blaz@kranjc.xyz>
2022-12-02 10:47:54 +0100

    Update call of function

--- Day2.hs
+++ Day2.hs
@@ -12,15 +12,15 @@ parse s = parse1 <$> lines s
     parseChar rock value = ord value - ord rock + 1
     parse1 l = (parseChar 'A' (l!!0), parseChar 'X' (l!!2))
 
-score1 :: (Int, Int) -> Int
-score1 (l, r) = (r - l + 1) `mod` 3 * 3 + r
+score1 :: Int -> Int -> Int
+score1 l r = (r - l + 1) `mod` 3 * 3 + r
 
-score2 :: (Int, Int) -> Int
-score2 (l, r) = (l + r) `mod` 3 + 1 + (r - 1) * 3
+score2 :: Int -> Int -> Int
+score2 l r = (l + r) `mod` 3 + 1 + (r - 1) * 3
 
 main :: IO ()
 main = do
   input <- readFile "data/02.txt"
   let games = parse input
-  print . sum $ score1 <$> games
-  print . sum $ score2 <$> games
+  print . sum $ uncurry score1 <$> games
+  print . sum $ uncurry score2 <$> games
