15d975f4686b13664d07b231911d247b0b8fe1fe
Blaz Kranjc <blaz@kranjc.xyz>
2022-12-03 11:57:24 +0100

    Sets, sets, sets, sets,...

--- Day3.hs
+++ Day3.hs
@@ -2,38 +2,30 @@
 {-
   stack script
   --resolver lts-20.2
+  --package containers
+  --package split
 -}
 
 import Data.Char
+import Data.List.Split
+import qualified Data.Set as Set
 
 priority :: Char -> Int
 priority c
   | isUpper c = ord c - ord 'A' + 27
   | otherwise = ord c - ord 'a' + 1
 
-duplicated :: Eq a => [a] -> [a] -> a
-duplicated _ [] = error "No duplicates"
-duplicated as (b:bs)
-  | b `elem` as = b
-  | otherwise = duplicated as bs
-
 splitHalf :: [a] -> ([a], [a])
 splitHalf as = splitAt ((length as + 1) `div` 2) as
 
-duplicated' :: Eq a => ([a], [a], [a]) -> a
-duplicated' (_, _, []) = error "No duplicates"
-duplicated' (as, bs, c:cs)
-  | c `elem` as && c `elem` bs = c
-  | otherwise = duplicated' (as, bs, cs)
-
-chunk3 :: [a] -> [(a, a, a)]
-chunk3 [] = []
-chunk3 as = (a, b, c) : chunk3 cs
-    where ([a, b, c], cs) = splitAt 3 as
+findCommon :: Ord a => [[a]] -> a
+findCommon aas = Set.elemAt 0 $ foldl commons (Set.fromList $ head aas) aas
+  where
+    commons acc as = acc `Set.intersection` Set.fromList as
 
 main :: IO ()
 main = do
   input <- readFile "data/03.txt"
   let backpacks = lines input
-  print $ sum $ priority . uncurry duplicated . splitHalf <$> backpacks
-  print $ sum $ priority . duplicated' <$> chunk3 backpacks
+  print $ sum $ priority . findCommon . (\(a, b) -> [a, b]) . splitHalf <$> backpacks
+  print $ sum $ priority . findCommon <$> chunksOf 3 backpacks
