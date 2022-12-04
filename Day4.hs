82a2473248779d8721adc2ce72d38da2958140af
Blaz Kranjc <blaz@kranjc.xyz>
2022-12-04 11:50:14 +0100

    Add split

--- Day4.hs
+++ Day4.hs
@@ -2,28 +2,25 @@
 {-
   stack script
   --resolver lts-20.2
+  --package split
 -}
 
-tokenize :: String -> [String] -> String -> [String]
-tokenize acc accs [] = reverse $ (reverse acc):accs
-tokenize acc accs (',':t) = tokenize [] ((reverse acc):accs) t
-tokenize acc accs ('-':t) = tokenize [] ((reverse acc):accs) t
-tokenize acc accs (h:t) = tokenize (h:acc) accs t
+import Data.List.Split
 
 parse :: String -> ((Int, Int), (Int, Int))
 parse s =
-  let [a, b, c, d] = tokenize [] [] s
+  let [a, b, c, d] = splitOneOf ",-" s
   in ((read a, read b), (read c, read d))
 
-fullyOverlaps :: ((Int, Int), (Int, Int)) -> Bool
-fullyOverlaps ((a, b), (c, d)) = ((a <= c) && (b >= d)) || ((c <= a) && (d >= b))
+fullyOverlaps :: (Int, Int) -> (Int, Int) -> Bool
+fullyOverlaps (a, b) (c, d) = ((a <= c) && (b >= d)) || ((c <= a) && (d >= b))
 
-overlaps :: ((Int, Int), (Int, Int)) -> Bool
-overlaps ((a, b), (c, d)) = ((c >= a) && (c <= b)) || ((a >= c) && (a <= d))
+overlaps :: (Int, Int) -> (Int, Int) -> Bool
+overlaps (a, b) (c, d) = ((c >= a) && (c <= b)) || ((a >= c) && (a <= d))
 
 main :: IO ()
 main = do
   input <- readFile "data/04.txt"
   let ranges = parse <$> lines input
-  print $ length $ filter fullyOverlaps ranges
-  print $ length $ filter overlaps ranges
+  print $ length $ filter (uncurry fullyOverlaps) ranges
+  print $ length $ filter (uncurry overlaps) ranges
