ea0642fe528c0c2d09f6d2eef741652211072134
Blaz Kranjc <blaz@kranjc.xyz>
2022-12-07 17:20:32 +0100

    Remove split, handle '/' properly

--- Day7.hs
+++ Day7.hs
@@ -3,28 +3,25 @@
   stack script
   --resolver lts-20.2
   --package containers
-  --package split
 -}
 
 import qualified Data.Map as Map
 import Data.List
-import Data.List.Split
 
 parse :: [String] -> [([String], Integer)]
 parse s = fst $ foldl update ([], []) s
   where
     update acc@(filesystem, stack) output
       | output == "$ cd .." = (filesystem, tail stack)
+      | output == "$ cd /" = (filesystem, [])
       | take 4 output == "$ cd" = (filesystem, drop 5 output:stack)
       | output == "$ ls" || take 3 output == "dir" = acc
-      | otherwise =
-        let [size, _] = splitOn " " output
-        in ((stack , read size):filesystem, stack)
+      | otherwise = ((stack , read $ takeWhile (/=' ') output):filesystem, stack)
 
 sizes :: [([String], Integer)] -> [Integer]
 sizes filesystem = snd <$> Map.toList (foldl updateMap Map.empty filesystem)
   where
-    parts dirs = flip drop dirs <$> [0..(length dirs - 1)]
+    parts dirs = flip drop dirs <$> [0..(length dirs)]
     updateMap m (dirs, size) = foldl (updateSize size) m (parts dirs)
     updateSize size m dir = Map.insert dir (Map.findWithDefault 0 dir m + size) m
 
