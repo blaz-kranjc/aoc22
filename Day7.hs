02055c17ccb31ef6a85ae2ceb3191f88125dafe4
Blaz Kranjc <blaz@kranjc.xyz>
2022-12-07 20:00:29 +0100

    Some style fixes

--- Day7.hs
+++ Day7.hs
@@ -9,17 +9,19 @@ import qualified Data.Map as Map
 import Data.List
 
 parse :: [String] -> [([String], Integer)]
-parse s = fst $ foldl update ([], []) s
+parse = fst . foldl update ([], [])
   where
-    update acc@(filesystem, stack) output
+    update (filesystem, stack) output
       | output == "$ cd .." = (filesystem, tail stack)
       | output == "$ cd /" = (filesystem, [])
       | take 4 output == "$ cd" = (filesystem, drop 5 output:stack)
-      | output == "$ ls" || take 3 output == "dir" = acc
-      | otherwise = ((stack , read $ takeWhile (/=' ') output):filesystem, stack)
+      | output == "$ ls" || take 3 output == "dir" = (filesystem, stack)
+      | otherwise =
+        let size = read . takeWhile (/=' ') $ output
+        in ((stack, size):filesystem, stack)
 
 sizes :: [([String], Integer)] -> [Integer]
-sizes filesystem = snd <$> Map.toList (foldl updateMap Map.empty filesystem)
+sizes = fmap snd . Map.toList . foldl updateMap Map.empty
   where
     parts dirs = flip drop dirs <$> [0..(length dirs)]
     updateMap m (dirs, size) = foldl (updateSize size) m (parts dirs)
@@ -28,7 +30,7 @@ sizes filesystem = snd <$> Map.toList (foldl updateMap Map.empty filesystem)
 main :: IO ()
 main = do
   input <- readFile "data/07.txt"
-  let filesystemSizes = sizes . parse $ lines input
-  print $ sum $ filter (<100000) filesystemSizes
+  let filesystemSizes = sizes . parse . lines $ input
+  print . sum . filter (<100000) $ filesystemSizes
   let available = 70000000 - maximum filesystemSizes
-  print $ minimum $ filter (\s -> available + s >= 30000000) filesystemSizes
+  print . minimum . filter (\s -> available + s >= 30000000) $ filesystemSizes
