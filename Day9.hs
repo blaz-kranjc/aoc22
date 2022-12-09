e5c07e537a5d37bbd20c00fad23c714556e49808
Blaz Kranjc <blaz@kranjc.xyz>
2022-12-09 20:14:10 +0100

    Decompress moves

--- Day9.hs
+++ Day9.hs
@@ -7,17 +7,16 @@
 
 import qualified Data.Set as Set
 
-parse :: String -> (Char, Int)
-parse s = (head s, read . drop 2 $ s)
+parse :: String -> [Char]
+parse s = replicate (read . drop 2 $ s) (head s)
 
-headPath :: [(Char, Int)] -> [(Int, Int)]
+headPath :: [Char] -> [(Int, Int)]
 headPath = reverse . fst . foldl update ([(0, 0)], (0, 0))
   where
-    update a (direction, 0) = a
-    update (visited, (x, y)) ('R', amount) = update ((x+1, y):visited, (x+1, y)) ('R', amount - 1)
-    update (visited, (x, y)) ('L', amount) = update ((x-1, y):visited, (x-1, y)) ('L', amount - 1)
-    update (visited, (x, y)) ('U', amount) = update ((x, y+1):visited, (x, y+1)) ('U', amount - 1)
-    update (visited, (x, y)) (c, amount) = update ((x, y-1):visited, (x, y-1)) (c, amount - 1)
+    update (visited, (x, y)) 'R' = ((x+1, y):visited, (x+1, y))
+    update (visited, (x, y)) 'L' = ((x-1, y):visited, (x-1, y))
+    update (visited, (x, y)) 'U' = ((x, y+1):visited, (x, y+1))
+    update (visited, (x, y)) c = ((x, y-1):visited, (x, y-1))
 
 tailVisited :: [(Int, Int)] -> [(Int, Int)]
 tailVisited = reverse . foldl update []
@@ -33,6 +32,6 @@ tailVisited = reverse . foldl update []
 main :: IO ()
 main = do
   input <- readFile "data/09.txt"
-  let headMotion = headPath . fmap parse . lines $ input
+  let headMotion = headPath . concatMap parse . lines $ input
   print . length . Set.fromList . tailVisited $ headMotion
   print . length . Set.fromList . flip (!!) 9 . iterate tailVisited $ headMotion
