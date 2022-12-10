a110bb6e67ea451848c5b319611b1cfb263dd50f
Blaz Kranjc <blaz@kranjc.xyz>
2022-12-10 11:53:18 +0100

    Refactoring

--- Day10.hs
+++ Day10.hs
@@ -6,7 +6,6 @@
   --package split
 -}
 
-import Control.Monad
 import Data.List.Split
 import qualified Data.Set as Set
 
@@ -15,22 +14,15 @@ parse s
   | s == "noop" = [0]
   | otherwise = [0, read . drop 5 $ s]
 
-display :: Set.Set Int -> IO ()
-display s = void (mapM putStrLn . chunksOf 40 . fmap pixel $ [0..239])
-  where
-    pixel v
-      | v `Set.member` s = '#'
-      | otherwise = '.'
-
-toPixels :: [Int] -> Set.Set Int
-toPixels = Set.fromList . fmap fst . filter (\v -> abs ((fst v `mod` 40) - snd v) <= 1) . zip [0..]
+pixel :: (Int, Int) -> Char
+pixel (i, v)
+  | abs (i `mod` 40 - v) <= 1 = '#'
+  | otherwise = '.'
 
 main :: IO ()
 main = do
   input <- readFile "data/10.txt"
-  let xs = scanl (+) 1 . concatMap parse .lines $ input
-  print xs
-  let times = [20 + i * 40 | i <- [0..5]]
-  print . sum . fmap (\t -> t * xs!!(t-1)) $ times
-  let pixels = toPixels xs
-  display pixels
+  let xs = zip [0..] . scanl (+) 1 . concatMap parse .lines $ input
+  let times = flip (-) 1 <$> [20 + i * 40 | i <- [0..5]]
+  print . sum . fmap ((\v -> (fst v + 1) * snd v) . (xs!!)) $ times
+  mapM_ putStrLn . chunksOf 40 . fmap pixel . take 240 $ xs
